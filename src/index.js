41b723442d8f4595ddc7dd16d0c9462f8d3d5857
sesychev <sergeysychou@gmail.com>
2022-07-10 22:19:03 +0300

    init: start human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+	const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+	'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+	const dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	const hundreds = 'hundred';
+
+	let str = '';
+	if (number <= 20) {
+		for (let index = 0; index < numbers.length; index++) {
+			if (number === index) {
+				str = numbers[index];
+			}
+		}
+	}
+
+	return str;
 }
